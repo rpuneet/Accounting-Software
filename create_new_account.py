1862d47af6715ab055be02ec2df97482406e40a8
Puneet <puneetrai04@gmail.com>
2018-01-03 23:43:15 +0530

    write some code

--- create_new_account.py
+++ create_new_account.py
@@ -4,4 +4,51 @@ Created on Wed Jan  3 22:52:50 2018
 
 @author: punee
 """
+import pandas as pd
+import numpy as np
+import os
+
+def get_user_information():
+    ''' Gets user information for storing in a csv file
+    
+            return type - pandas Series
+        '''
+        
+    index_of_details = ['First Name' , 'Lase Name' , 'e-mail id' , 'User Id' , 'Password']
+    
+    user_details = []
+    
+    for index in index_of_details:
+        user_details.append(input("{} : ".format(index)))
+    
+    return pd.Series(user_details, index = index_of_details)
+    
+def save_user_details(user_information):
+    ''' Stores the data in a new file
+    
+        return type - none
+    '''
+    
+    loction_to_store_details = os.getcwd()+"\\Database-Management\\user-accounts-details\\"
+    filename = "userdetails"
+    fileno = 0
+    final_location = loction_to_store_details + filename + str(fileno)
+    while(os.path.isfile(final_location)):
+        fileno+=1
+        
+    user_information.to_csv(final_location)
+    
+def create_new_account():
+    ''' Creates a new account
+            stores user information in Database-Management/user-account-details
+            password is stored as sha3_256 hash
+            
+            return type - none
+        '''
+    
+    user_information = get_user_information()
+    
+    save_user_details(user_information)
+    
+
 
--- home.py
+++ home.py
@@ -4,6 +4,7 @@ Created on Wed Jan  3 22:26:06 2018
 
 @author: rPuneet
 """
+from create_new_account import *
 from login import *
 
 
@@ -11,7 +12,7 @@ def get_option():
     ''' Gets choice from user for different options 
             
             return type - int
-                (choice number or -1 if choice is invalid)
+                (option_number)
     '''
     print("1. Login.\n2. Create New Account.")
     option_number = int(input("Enter Your option :"))
@@ -31,4 +32,9 @@ def initialise():
     '''
     option_type = get_option()
     
+    if option_type == 1 :
+        login()
+    else :
+        create_new_account()
+        
     
